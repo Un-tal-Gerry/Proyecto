1e8eba1e9359d320ab56ddba4ac037dcc54b59d6
Un-tal-Gerry <75149655+Un-tal-Gerry@users.noreply.github.com>
2020-11-27 22:26:29 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html><!-- es una instrucción especial que va al inicio de nuestro documento HTML, damos el inicio con el documento-->
+<html lang="en">
+<head>
+	<meta charset="UTF-8"><!--se incorporan en el encabezado de una página web y que resultan invisibles para un visitante normal-->
+	<title> PROYECTO </title><!--cerramos lo declarado-->
+	 <link rel="stylesheet" type="text/css" href="tablas.css"/><!--especifica la relación entre el documento actual y un recurso externo-->
+</head><!--cerramos lo declarado-->
+
+<body>	
+	<center>
+		<td><font color="#FFFFFF" face=" helvetica"><!--Indica el tamaño, color, o fuente del texto que contiene clartamente se puede ver-->
+		<h1> Lenguaje de Programaciòn  </h1><!-- el título de un pos-->
+	</center>
+
+	<center>		
+				<a href=""><img src="1.png"width= "8%"; height= "auto%"></a> 
+				<p style="font-size: 18px;"><b>¿Qué es Java?</b></p><!--aqui tanto damos la propiedad sea distribuida en otros parrafos, pero tambien es representado-->
+				<p style="font-size: 18px;"><b>Java es una plataforma informática y a su vez un lenguaje de programación creado en 1995 por la empresa Sun Microsystem.</b></p><!-- lo mismo que  lo anterior-->
+				
+	</center><!--cerramos lo declarado-->
+
+	<table>
+<caption>Caracteristicas mas Importantes</caption>
+  <tr>
+    <th>Nª </th>
+    <th>Caracteristicas</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>Orientado a Objetos</td>
+  </tr>
+   <tr>
+    <td>2</td>
+    <td>Multiplataforma</td>
+  </tr>
+   <tr>
+    <td>3</td>
+    <td>Portable</td>
+  </tr>
+   <tr>
+    <td>4</td>
+    <td>Seguro</td>
+  </tr>
+   <tr>
+    <td>5</td>
+    <td>Multihilo</td>
+  </tr>
+
+</table>
+
+<br><!--saltos de linea-->
+
+
+</body><!--cerramos lo declarado-->
+</html><!--cerramos lo declarado-->
